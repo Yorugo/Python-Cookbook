666ed4521fe7876399492008e2085655e5f8479f
Atheridis Georgios <giorgosatheridis@gmail.com>
2020-11-24 02:15:05 +0200

    Committing user input program

---
+++ user_input.py
@@ -0,0 +1,7 @@
+print("Hello\n")
+name = input("What is your name?\nName: ")
+print(f"Hello {name}")
+
+number = input("What is your favourite number?\nPlease enter your number: ")
+triple = int(number) * 3
+print(f"Your number multiplied by three is {triple}")
